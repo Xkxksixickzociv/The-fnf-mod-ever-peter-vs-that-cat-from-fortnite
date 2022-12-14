9f6d53ed6d8fc5eb98b4aeafe41189c17d03ed93
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2022-12-14 21:26:33 +0200

    Add files via upload

---
+++ assets/preload/stages/meow.lua
@@ -0,0 +1,9 @@
+function onCreate()
+        makeLuaSprite('hi','fortnit/fort',-40, 900)
+        addLuaSprite('hi',false)
+
+end
+ function onUpdate(elpased)
+    setProperty('gf.visible', false);
+
+end
